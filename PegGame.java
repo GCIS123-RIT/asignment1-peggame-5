3a6ed5eaff190653bbeccb1401ff4020f95a6db2
Rex Hugo Marciano <108262745+r3ximous@users.noreply.github.com>
2024-03-06 13:35:48 +0400

    Update PegGame.java

--- PegGame.java
+++ PegGame.java
@@ -1,9 +1,11 @@
 package main.java.Assignment_1;
 
+import java.util.Collection;
+
 public interface PegGame {
     public static final int BOARD_SIZE = 5;
 
-    public abstract void getPossibleMoves(Location loc);
+    public Collection<Move> getPossibleMoves();
 
     public GameState getGameState();
 

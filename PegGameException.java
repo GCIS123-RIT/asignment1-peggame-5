708ebf32025d68d1e9f275a8f0d47a73f0fd2edc
Rex Hugo Marciano <108262745+r3ximous@users.noreply.github.com>
2024-03-07 20:07:48 +0400

    Update and rename pegGameException.java to PegGameException.java

---
+++ PegGameException.java
@@ -0,0 +1,6 @@
+public class PegGameException extends RuntimeException {
+    public  PegGameException (String message ){
+        System.out.println(message);
+
+    }
+}
--- pegGameException.java
+++
@@ -1,6 +0,0 @@
-public class pegGameException extends RuntimeException {
-    public  pegGameException (String message ){
-        System.out.println(message);
-
-    }
-}

b38b5bb679e67153253e61291115f85f47f7a1a8
amir132334 <144325432+amir132334@users.noreply.github.com>
2024-03-08 22:00:22 +0400

    Update LocationTest.java

--- LocationTest.java
+++ LocationTest.java
@@ -1,1 +1,36 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.tools.DocumentationTool.Location;
+
+import org.junit.jupiter.api.Test;
+@Testable
+public class LocationTest {
+
+    @Test
+    void testConstructorAndGetters() {
+        Location expecteLocation = new Location(2, 3);
+        Integer ExpectedRow = 2;
+        Integer ExpectedColumn = 3;
+        assertEquals(ExpectedRow, excpectedlocation.getRow());
+        assertEquals(ExpectedColumn, expectedlocation.getCol());
+    }
+
+    @Test
+    void testToString() {
+        Location expected = new Location(4, 5);
+        String Actual = "Row: 4 Col: 5";
+        assertEquals(Actual, expected.toString());
+        
+    }
+
+    @Test
+    void testEquals() {
+        Location location1 = new Location(2, 3);
+        Location location2 = new Location(2, 3);
+        Location location3 = new Location(3, 2);
+
+        assertTrue(location1.equals(location2)); // Same row and col
+        assertFalse(location1.equals(location3)); // Different row and col
+    }
+}
 

90a5180fcb577ad4db42a69bd8424d3f6310dfd0
Rex Hugo Marciano <108262745+r3ximous@users.noreply.github.com>
2024-03-08 08:54:26 +0000

    Add cmd_line.java and required libraries

---
+++ cmd_line.java
@@ -0,0 +1,21 @@
+import java.util.Collection;
+
+public class cmd_line implements PegGame {
+    
+    public Collection<Move> getPossibleMoves(){
+        return null;
+    }
+
+    public GameState getGameState(){
+        return null;
+    }
+
+    public void makeMove(Move move) throws PegGameException{
+        return;
+    }
+
+    //Include a static method that, given any instance of the PegGame interface will allow a user to enter commands into standard input and play until completion or until they quit.
+    public static void main(String[] args){
+        return;
+    }
+}

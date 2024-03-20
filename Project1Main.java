81ebf508a30a803bfbaaaf728becba898a812469
Rex Hugo Marciano <108262745+r3ximous@users.noreply.github.com>
2024-03-20 13:00:39 +0400

    Update Project1Main.java

--- Project1Main.java
+++ Project1Main.java
@@ -1,6 +1,8 @@
+package main.java.Assignment1;
+
 import java.util.Scanner;
 public class Project1Main {
-    /*
+    /**
      * This is the main method that will read in a file and play the game.
      * 
      * @param args the command line arguments
@@ -10,20 +12,19 @@ public class Project1Main {
      * 
      * @return the current state of the game
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         Scanner scanner = new Scanner(System.in);
         
         System.out.print("Enter a filename: ");
-        String filename = scanner.nextLine();
+        String filename = scanner.nextLine(); 
         
-        cmd_line game = ReadTxt.createGameFromFile(filename);
-        
-        if (game == null) {
-            System.out.println("Error reading file");
-        }
+        ReadTxt reader = new ReadTxt(filename); // Create an instance of ReadTxt
+        boolean[][] board = reader.readFromFile(); // Read the file and store the information in a 2d array
 
-        cmd_line.playGame(game);
-        
+        cmd_line game = new cmd_line(board); // Create an instance of cmd_line
+
+        game.playGame(game); // Play the game
+    
         scanner.close();
     }
 }

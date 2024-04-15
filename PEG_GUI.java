bc4d60ccaeccda82cb8bf8249e3ef5fc7c68b7bf
amir132334 <144325432+amir132334@users.noreply.github.com>
2024-04-15 23:51:21 +0400

    Create PEG_GUI.java

---
+++ PEG_GUI.java
@@ -0,0 +1,20 @@
+package PegGame;
+import java.io.File;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+public class PEG_GUI {
+
+    public static void loadGame (){
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Peg Game File");
+        File selectedFile = fileChooser.showOpenDialog(PegUI.getMainStage());
+        
+        // Now you can use selectedFile to access the chosen file
+    } 
+
+    public static void exitGame (){
+        System.exit(0);
+    } 
+}

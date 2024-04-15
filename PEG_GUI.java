dd358cf5f83a7e697fec47a70759751d02bd53f5
Rex Hugo Marciano <108262745+r3ximous@users.noreply.github.com>
2024-04-16 01:00:42 +0400

    Update PEG_GUI.java to loadGame

--- PEG_GUI.java
+++ PEG_GUI.java
@@ -10,7 +10,17 @@ public class PEG_GUI {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Open Peg Game File");
         File selectedFile = fileChooser.showOpenDialog(PegUI.getMainStage());
-        
+        if (selectedFile != null) {
+            ReadTxt reader = new ReadTxt(selectedFile.getAbsolutePath()); // Create an instance of ReadTxt
+            try {
+                boolean[][] board = reader.readFromFile(); //from ReadTxt.java
+                cmd_line game = new cmd_line(board);
+            } catch (Exception e) {
+                Label statusLabel = new Label();
+                statusLabel.setText("Error loading game file.");
+                
+            }
+        }
         // Now you can use selectedFile to access the chosen file
     } 
 

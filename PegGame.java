f1a423f00a5ceea8d37f46c7bb904325ab6a63d4
Rex Hugo Marciano <108262745+r3ximous@users.noreply.github.com>
2024-04-17 12:19:54 +0400

    Update PegGame.java

--- PegGame.java
+++ PegGame.java
@@ -2,12 +2,30 @@ package main.java.Assignment1;
 
 import java.util.Collection;
 
+/**
+ * PegGame interface
+ */
 public interface PegGame {
 
-    public Collection<Location> getPossibleMoves();
+    /**
+     * Get the current location of the empty hole
+     * 
+     * @return the location of the empty hole
+     */
+    public Collection<Location> getPossibleMoves(Location loc);
 
+    /**
+     * determine the gamestate of the peg game and actions based off condition
+     * 
+     * @return the gamestate of the peg game
+     */
     public GameState getGameState();
 
+    /**
+     * make a move in the peg game
+     * 
+     * @param move the move to make
+     */
     public void makeMove(Move move) throws PegGameException;
 
 }
